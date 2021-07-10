91a73fae425b711313c379ae2b5c4c232fa966ca
Mr3O5 <46283095+Mr3O5@users.noreply.github.com>
2021-07-10 14:17:21 +0200

    responsive

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ container.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
   //Popout
   title.style.transform = "translateZ(150px)";
-  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-30deg)";
   description.style.transform = "translateZ(125px)";
   sizes.style.transform = "translateZ(100px)";
   purchase.style.transform = "translateZ(75px)";
