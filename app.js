aee2a953bffea9f1587fe22466ad8afaa95f8a49
Mr3O5 <46283095+Mr3O5@users.noreply.github.com>
2021-07-10 14:03:54 +0200

    change script data at html

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-//Movement Animation
+//Movement Animation to happen
 let card = document.querySelector(".card");
 let container = document.querySelector(".container");
 
-//Items Move
+//Items
 let title = document.querySelector(".title");
 let sneaker = document.querySelector(".sneaker img");
 let purchase = document.querySelector(".purchase");
@@ -11,34 +11,32 @@ let sizes = document.querySelector(".sizes");
 
 //Moving Animation Event
 container.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-  });
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
 
-// animation In
+
+//Animate In
 container.addEventListener("mouseenter", (e) => {
-    card.style.transition = "none";
-
-    title.style.transform = "translateZ(150px)";
-    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
-    description.style.transform = "translateZ(125px)";
-    sizes.style.transform = "translateZ(100px)";
-    purchase.style.transform = "translateZ(75px)";
-
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(75px)";
 });
 
-//animation out
 
+//Animate Out
 container.addEventListener("mouseleave", (e) => {
-    card.style.transition = "all 0.5s ease";
-    card.style.transition = `rotateY(0deg) rotateX(0deg)`;
-
-    title.style.transform = "translateZ(0px)";
-    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
-    description.style.transform = "translateZ(0px)";
-    sizes.style.transform = "translateZ(0px)";
-    purchase.style.transform = "translateZ(0px)";
-
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
 });
-
